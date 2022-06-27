9f1bc203c52ce1357fdc17c2db95e751793e4751
OleanjiKingCode <holamilean56@gmail.com>
2022-06-27 10:11:05 +0100

    graph

--- constant.js
+++ constant.js
@@ -1,5 +1,5 @@
 
   export const MemeForestAddress ="0x226268B7043C33BBF7309446D72101780B75c94E"
   export const Token =" eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDViNjY5YzI2NTQ5NTUzQTUwOWNkNTNhMGQ2QjgyYTg1QTcwNTFEQjQiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NTYxODkzNjUwNzMsIm5hbWUiOiJTb2NpYWwtQXJ0LUZvcmVzdCJ9.CDFYo8faCU5-eIECVKuALVSDLVgkyaB4r5NbcxOmGxU"
-  
+  export const ApiUriv = "https://api.thegraph.com/subgraphs/name/oleanjikingcode/newnftmemes"
   
--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,33 @@ import Web3Modal from "web3modal";
 import styles from '../styles/Home.module.css'
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 import { useContract, useProvider,useSigner,useAccount,useBalance,useConnect  } from 'wagmi'
-import {MemeForestAddress} from '../constant'
+import {MemeForestAddress,ApiUriv} from '../constant'
 import { useEffect, useRef, useState, useContext } from "react";
 import BigNumber from 'bignumber.js';
 import MEME from '../artifacts/contracts/MemeForest.sol/MemeForest.json'
 import { FaSpinner } from 'react-icons/fa';
-import { HiMenu } from "react-icons/hi";
+import { createClient } from 'urql'
 
-export default function Home() {
+const MemberQuery= `
+query {
+ 
+  memebers{
+   
+    Adddress
+    
+  }
+}
+`
+
+const client = createClient({
+  url: ApiUriv,
+})
+
+
+
+export default function Home(props) {
+  console.log(props)
+  const array = props.members
   const { data} = useAccount()
   const person = data?.address;
 
@@ -38,17 +57,13 @@ export default function Home() {
       signerOrProvider: provider,
     })
     const balance = 1;
-  useEffect(() => {
+  // useEffect(() => {
     
-        PageLoad();
-        if(!AMember){
-           checkIfAMember();
-        
-        //  setInterval( async () => {
-        //      await fetchByAddress()
-        //  }, 5*1000);
-        }
-    }, [AMember]);
+  //       PageLoad();
+  //       if(!AMember){
+  //          checkIfAMember();
+  //       }
+  //   }, [AMember]);
 
     const PageLoad = async () =>{
       try {
@@ -75,165 +90,169 @@ export default function Home() {
   }
 
 
-   const fetchByAddress = async () => {
-    try {
-      const data= await contractWithProvider.fetchMembers();
+  //  const fetchByAddress = async () => {
+  //   try {
+  //     const data= await contractWithProvider.fetchMembers();
     
-      const tx = await Promise.all(data.map(async i =>{
-         let list = {
-          Name : i.Name,
-          Address : i.MemeberAddress,
-          Date: i.Datejoined,
-          Memes : i.MyMemes.toNumber(),
-          Starred :i.MyStarredMemes.toNumber()
-        }
-        return list
-       }));
-       setMemberDetails(tx)
-     } catch (w) {
-       console.log(w)
-     }
-   }
+  //     const tx = await Promise.all(data.map(async i =>{
+  //        let list = {
+  //         Name : i.Name,
+  //         Address : i.MemeberAddress,
+  //         Date: i.Datejoined,
+  //         Memes : i.MyMemes.toNumber(),
+  //         Starred :i.MyStarredMemes.toNumber()
+  //       }
+  //       return list
+  //      }));
+  //      setMemberDetails(tx)
+  //    } catch (w) {
+  //      console.log(w)
+  //    }
+  //  }
 
-  const checkIfAMember = async () => {
-    try {
+  // const checkIfAMember = async () => {
+  //   try {
      
-      const tx= await contractWithProvider.IsAMember(person)
+  //     const tx= await contractWithProvider.IsAMember(person)
      
     
-      if(tx) {
+  //     if(tx) {
      
-        setAMember(true)
-      }
-      else{
-        setAMember(false)
-      }
+  //       setAMember(true)
+  //     }
+  //     else{
+  //       setAMember(false)
+  //     }
       
-    } catch (e) {
-      console.log(e)
-      setAMember(false)
-    }
-  }
+  //   } catch (e) {
+  //     console.log(e)
+  //     setAMember(false)
+  //   }
+  // }
 
   
 const renderButton = () => {
 
-  if (AMember && !haveInitialised) {
+  <div>
+
+  
+  {/* // if (AMember && !haveInitialised) {
     
-    return(
-     <div style={{textAlign:"center",height:"80vh",top:"50%", left:"50%", display:"flex", alignItems:"center",justifyContent:"center" ,flexDirection:"column"}}>
-       <h3 className={styles.title}>
-          Welcome to Meme Forest
-        </h3>
-        <br/>
+  //   return(
+  //    <div style={{textAlign:"center",height:"80vh",top:"50%", left:"50%", display:"flex", alignItems:"center",justifyContent:"center" ,flexDirection:"column"}}>
+  //      <h3 className={styles.title}>
+  //         Welcome to Meme Forest
+  //       </h3>
+  //       <br/>
              
-        </div>
-    )
-  }
+  //       </div>
+  //   )
+  // }
 
-  if(AMember) {
-    return(
-      <div style={{fontSize:"19px", fontWeight:"700"}}>
-        You are a member with Funding balance of {balance}
-        <br/> <br/>
-        {
-          memberDetails.map((lists,i) => {
+  // if(AMember) {
+  //   return(
+  //     <div style={{fontSize:"19px", fontWeight:"700"}}>
+  //       You are a member with Funding balance of {balance}
+  //       <br/> <br/>
+  //       {
+  //         memberDetails.map((lists,i) => {
             
-            return(
+  //           return(
                 
-                <div key={i}  style={{fontSize:"20px", fontWeight:"700"}}>
-                  {
-                    lists.Address == person &&
-                    <div>
-                    <div style={{ padding:"30px 10px", display:"flex", alignItems:"center"}} > 
-                    Name: 
-                   <span style={{fontSize:"18px" ,fontWeight:"400", marginTop:"5px " ,marginLeft:"20px"}}>
-                    {lists.Name}
-                    </span>
+  //               <div key={i}  style={{fontSize:"20px", fontWeight:"700"}}>
+  //                 {
+  //                   lists.Address == person &&
+  //                   <div>
+  //                   <div style={{ padding:"30px 10px", display:"flex", alignItems:"center"}} > 
+  //                   Name: 
+  //                  <span style={{fontSize:"18px" ,fontWeight:"400", marginTop:"5px " ,marginLeft:"20px"}}>
+  //                   {lists.Name}
+  //                   </span>
                    
-                   </div>
-                   <div style={{ padding:"30px 10px"}}> 
-                    Address: 
-                    <span style={{fontSize:"18px" ,fontWeight:"400", marginTop:"5px " ,marginLeft:"20px"}}>
-                    {lists.Address}
-                    </span>
-                   </div>
-                   <div style={{ padding:"30px 10px"}}> 
-                     Number of Uploads: 
-                     <span style={{fontSize:"18px" ,fontWeight:"400", marginTop:"5px " ,marginLeft:"20px"}}>
-                     {lists.Memes}
-                     </span>
-                   </div>
-                   <div style={{ padding:"30px 10px"}}>
-                     Number Of Starred Memes:
-                     <span style={{fontSize:"18px" ,fontWeight:"400", marginTop:"5px " ,marginLeft:"20px"}}>
-                      {lists.Starred} </span>
-                    </div>
-                   <div style={{ padding:"30px 10px"}}> 
-                    Date Joined: 
-                    <span style={{fontSize:"18px" ,fontWeight:"400", marginTop:"5px " ,marginLeft:"20px"}}>
-                    {lists.Date} </span>
-                   </div>
-                    </div>
-                  }
+  //                  </div>
+  //                  <div style={{ padding:"30px 10px"}}> 
+  //                   Address: 
+  //                   <span style={{fontSize:"18px" ,fontWeight:"400", marginTop:"5px " ,marginLeft:"20px"}}>
+  //                   {lists.Address}
+  //                   </span>
+  //                  </div>
+  //                  <div style={{ padding:"30px 10px"}}> 
+  //                    Number of Uploads: 
+  //                    <span style={{fontSize:"18px" ,fontWeight:"400", marginTop:"5px " ,marginLeft:"20px"}}>
+  //                    {lists.Memes}
+  //                    </span>
+  //                  </div>
+  //                  <div style={{ padding:"30px 10px"}}>
+  //                    Number Of Starred Memes:
+  //                    <span style={{fontSize:"18px" ,fontWeight:"400", marginTop:"5px " ,marginLeft:"20px"}}>
+  //                     {lists.Starred} </span>
+  //                   </div>
+  //                  <div style={{ padding:"30px 10px"}}> 
+  //                   Date Joined: 
+  //                   <span style={{fontSize:"18px" ,fontWeight:"400", marginTop:"5px " ,marginLeft:"20px"}}>
+  //                   {lists.Date} </span>
+  //                  </div>
+  //                   </div>
+  //                 }
                   
-                </div>
-            ) 
-          })
+  //               </div>
+  //           ) 
+  //         })
                    
-        }
-      </div>
-    )
-  }
+  //       }
+  //     </div>
+  //   )
+  // }
 
-  if(!AMember){
-    return (
+  // if(!AMember){
+  //   return (
 
-      <div>
-          {
-          loadingpage ? 
-          ( 
-              <div style={{fontSize:"100px", textAlign:"center"}}>
-                  <FaSpinner icon="spinner" className={styles.spinner} />
-              </div>
-          ) 
-          : 
-          (
-            <div style={{textAlign:"center",height:"80vh",top:"50%", left:"50%", display:"flex", alignItems:"center",justifyContent:"center" ,flexDirection:"column"}}>
-              <h2 style={{}}>
-                Welcome To MemeForest
-              </h2>
-              <input
-                placeholder='Enter Any Name'
-                type="text"
-                onChange={e => setName(e.target.value)}
-                style={{padding:"10px", border:"1px solid black" , borderRadius:"10px",width:"400px", fontSize:"10px"}}
-              />
+  //     <div>
+  //         {
+  //         loadingpage ? 
+  //         ( 
+  //             <div style={{fontSize:"100px", textAlign:"center"}}>
+  //                 <FaSpinner icon="spinner" className={styles.spinner} />
+  //             </div>
+  //         ) 
+  //         : 
+  //         (
+  //           <div style={{textAlign:"center",height:"80vh",top:"50%", left:"50%", display:"flex", alignItems:"center",justifyContent:"center" ,flexDirection:"column"}}>
+  //             <h2 style={{}}>
+  //               Welcome To MemeForest
+  //             </h2>
+  //             <input
+  //               placeholder='Enter Any Name'
+  //               type="text"
+  //               onChange={e => setName(e.target.value)}
+  //               style={{padding:"10px", border:"1px solid black" , borderRadius:"10px",width:"400px", fontSize:"10px"}}
+  //             />
               
-              {
-                loading? 
-                  (
-                    <button   style={{border:"none", textAlign:"center", 
-                        padding:"10px 20px",color:"white",  fontSize:"10px", 
-                        backgroundColor:"blue",marginTop:"20px", marginLeft:"20px", borderRadius:"10px"}}>
-                        <FaSpinner icon="spinner" className={styles.spinner} />
-                    </button>
-                  ) 
-                  :
-                  (
-                    <button onClick={joinMembership}  style={{border:"none", textAlign:"center", 
-                      padding:"10px 20px",color:"white",  fontSize:"10px", 
-                      backgroundColor:"blue",marginTop:"20px", marginLeft:"20px", borderRadius:"10px"}}>
-                        Become A Member
-                    </button>    
-                  )
-              }       
-          </div>
-          )
-        }
-      </div>
-    )
-  }
+  //             {
+  //               loading? 
+  //                 (
+  //                   <button   style={{border:"none", textAlign:"center", 
+  //                       padding:"10px 20px",color:"white",  fontSize:"10px", 
+  //                       backgroundColor:"blue",marginTop:"20px", marginLeft:"20px", borderRadius:"10px"}}>
+  //                       <FaSpinner icon="spinner" className={styles.spinner} />
+  //                   </button>
+  //                 ) 
+  //                 :
+  //                 (
+  //                   <button onClick={joinMembership}  style={{border:"none", textAlign:"center", 
+  //                     padding:"10px 20px",color:"white",  fontSize:"10px", 
+  //                     backgroundColor:"blue",marginTop:"20px", marginLeft:"20px", borderRadius:"10px"}}>
+  //                       Become A Member
+  //                   </button>    
+  //                 )
+  //             }       
+  //         </div>
+  //         )
+  //       }
+  //     </div>
+  //   )
+  // } */}
+  </div>
 }
 
 
@@ -259,7 +278,38 @@ const renderButton = () => {
     </div>
       <div  className={styles.main}> 
         
-          {renderButton()}
+      {
+        // props.members.map((add,i) => {
+        //   // console.log(person, add)
+        //   return(
+        //     <div key={i}>
+        //       {
+        //         (person == add.Adddress ) ?
+                
+        //           ( 
+        //               <div style={{fontSize:"19px", fontWeight:"700"}}>
+        //                 You are a member
+        //                 {
+        //                   console.log("dkfnjiafnav",add.Adddress)
+        //                 }
+        //                 <br/> <br/>
+        //             </div>
+        //           ) 
+        //           :
+        //           (
+        //             <div style={{fontSize:"19px", fontWeight:"700"}}>
+        //               You are a not member
+        //               <br/> <br/>
+        //           </div>
+        //           )
+        //       }
+        //     </div>
+        //   )
+        // })
+        
+       
+      }
+
         
 
       </div>
@@ -267,4 +317,21 @@ const renderButton = () => {
       
     </div>
   )
+}
+
+
+async function GetData() {
+  const data = await client.query(MemberQuery).toPromise()
+  console.log(data)
+  return data.data.memebers
+}
+
+
+export async function getServerSideProps() {
+  const data = await GetData()
+  return{
+    props:{
+      members:data
+    }
+  }
 }
